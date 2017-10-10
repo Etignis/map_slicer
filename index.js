1eda3017017fdeeaf7462effadd5138b1f8267d2
Tenebrius <etignis@gmail.com>
2017-10-10 16:13:55 +0300

    wip

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cp = require('child_process');
 
 const sPathToSrc = 'myImage.jpg'; // fix
 const sPathToOut = 'map/{z}/{y}_{x}.png'; // fix
+const nTileSize = 256;
 
 function sliceImage(sInPath, sOutPath, nZ) {
   // The parameters passed here are equal to the command-line parameters
@@ -43,15 +44,17 @@ function sliceImage(sInPath, sOutPath, nZ) {
 
 function resizeImage(sPath, sFileName, oParams) {
   if (path.extname(file) === jpgExt && !/__\d+.jpg/.test(file)) {
+    const nZoomLevel = oParams.nZ;
+    const nImgSize = nTileSize * nZoomLevel;
     const sSrcPath = path.join(sPath, file);
     const fileName = path.basename(file, jpgExt);
     console.log("Resize \""+sSrcPath+"\"");
-    const nSize = oParams.size;
+    //const nSize = oParams.size;
 
-    const sNewPath = path.join(sPath, path.parse(file).dir, fileName+"__"+nSize+".jpg");
+    const sNewPath = path.join(sPath, path.parse(file).dir, fileName+"__"+nImgSize+".jpg");
      // console.log("Try to convert into \""+sNewPath+"\"");
 
-    cp.exec(`magick "${sSrcPath}" -resize ${nSize} "${sNewPath}"`)
+    cp.exec(`magick "${sSrcPath}" -resize ${nImgSize} "${sNewPath}"`)
 
 
   }
@@ -61,8 +64,9 @@ function loopImages() {
   var aZ = [0,1,2,3,4,5,6,7];
 
   aZ.forEach(function(nZoom){
-    resizeImage(sPath, sFileName, oParams);
-    sliceImage(sInPath, sOutPath);
+    resizeImage(sPath, sFileName, {nZ: nZoom});
+    sliceImage(sInPath, sOutPath, nZoom);
+    // when resizeImage than  sliceImage
   });
 
 }
